80c7e5e349e2cd8e45d76ffd13136296ba9e9a7f
Ionut Sanda <sandaionutflorin@yahoo.com>
2019-03-31 16:35:40 +0300

    Main class

---
+++ src/main/MainClass.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Ionut on 3/31/2019.
+ */
+public class MainClass {
+
+    public static void main(String... args) {
+        System.out.println("Test");
+    }
+}
